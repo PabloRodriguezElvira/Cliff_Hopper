5f81873381234d93f7bcfaf2f988e76f5c0c6e6a
unknown <pablo.rodriguez.elvira@estudiantat.upc.edu>
2023-05-16 18:44:31 +0200

    creado vector de placas y giro

--- Assets/Scripts/CreateLevel.cs
+++ Assets/Scripts/CreateLevel.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 
@@ -8,90 +9,68 @@ enum TerrainType { Grass, Road };
 
 public class CreateLevel : MonoBehaviour
 {
-    readonly float[] terrainProb = { 0.85f, 0.85f };
-
-    public GameObject grassPrefab, roadPrefab;
+    public Vector3[] Placas;
     public GameObject bloke;
     public GameObject placa;
-    public float minSpeed, maxSpeed;
-    //
-    //Start is called before the first frame update
-    /*void Start()
-    {
-        GameObject obj;
-        for (uint i = 0; i < 15; i++)
-        {
-            obj = (GameObject)Instantiate(grassPrefab);
-            obj.transform.Translate(0.0f, 0.0f, i - 10.0f);
-            obj.transform.parent = transform;
-        }
-        TerrainType type = TerrainType.Road;
-        uint size;
-        float value, z = 5.0f;
-        for (uint i = 5; i < 100; i++)
-        {
-            value = Random.value;
-            if (value < terrainProb[(int)type])
-                size = 1;
-            else
-                size = 5;
-            for (uint j = 0; j < size; j++)
-            {
-                obj = (GameObject)Instantiate((type == TerrainType.Grass) ? grassPrefab : roadPrefab);
-                obj.transform.Translate(0.0f, 0.0f, z);
-                if (Random.value < 0.5f)
-                {
-                    obj.transform.Rotate(0.0f, 180.0f, 0.0f);
-                }
-                obj.transform.parent = transform;
-                if (type == TerrainType.Road)
-                    obj.transform.GetChild(0).GetComponent<CreateCar>().speed = minSpeed + Random.value * (maxSpeed - minSpeed);
-                z += 1.0f;
-            }
-            type = (TerrainType)(((int)type + 1) % 2);
-        }
-    }*/
+
     void Start()
     {
         GameObject obj;
+        Placas = new Vector3[20];
+        int k = 0;
         float sizeBlock = 4.0f;
         float sizePlaca = 3.0f;
         int blocks = 4;
 
+        Vector3 position;
+
         //Tiles en línea recta:
         for (int i = 0; i < 4; ++i)
         {
+            position = new Vector3(0.0f, -4.0f, i * sizeBlock);
             obj = (GameObject)Instantiate(bloke);
-            obj.transform.Translate(0.0f, -4.0f, i * sizeBlock);
+            obj.transform.Translate(position);
             obj.transform.parent = transform;
         }
         //Giradas
         for (int i = 0; i < 4; ++i)
         {
+            position = new Vector3(i * sizeBlock, -4.0f, 4.0f * sizeBlock);
             obj = (GameObject)Instantiate(bloke);
-            obj.transform.Translate(i*sizeBlock, -4.0f, 4.0f*sizeBlock);
+            obj.transform.Translate(position);
             obj.transform.parent = transform;
             
             //Placa:
             if (i == 0)
             {
+                position = new Vector3(i * sizeBlock, 0.0f, 4.0f * sizePlaca);
+                Placas[k] = position; ++k;
+                Debug.Log(position);
+
                 obj = (GameObject)Instantiate(placa);
-                obj.transform.Translate(i*sizeBlock, 0.0f, 4.0f*sizePlaca);
+                obj.transform.Translate(position);
                 obj.transform.parent = transform;
             }
         }
         //Tiles en línea recta:
         for (int i = 4; i < 8; ++i)
         {
+            position = new Vector3(blocks * sizeBlock, -4.0f, i * sizeBlock);
+            obj = (GameObject)Instantiate(bloke);
+            obj.transform.Translate(position);
+            obj.transform.parent = transform;
+
+            //Placa
             if (i == 4)
             {
+                position = new Vector3(blocks * sizeBlock, 0.0f, i * sizePlaca);
+                Debug.Log(position);
+                Placas[k] = position; ++k;
+
                 obj = (GameObject)Instantiate(placa);
-                obj.transform.Translate(blocks*sizeBlock, 0.0f, i*sizePlaca);
+                obj.transform.Translate(position);
                 obj.transform.parent = transform;
             }
-            obj = (GameObject)Instantiate(bloke);
-            obj.transform.Translate(blocks*sizeBlock, -4.0f, i * sizeBlock);
-            obj.transform.parent = transform;
         }
     }
 }
